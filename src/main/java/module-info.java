f01abb10092095f88652ea2e8259e5507814c897
alvaroatue <atuealvaro@gmail.com>
2024-06-02 18:46:28 -0300

    a
    a

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.tpfinal {
     requires de.jensd.fx.glyphs.fontawesome;
    /// requires java.sql;
     requires org.xerial.sqlitejdbc;
+    requires java.desktop;
     opens org.example.TableTracker to javafx.fxml;
     exports org.example.TableTracker;
     exports org.example.TableTracker.Controllers;
